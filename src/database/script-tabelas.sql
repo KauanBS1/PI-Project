04cecbc8a69c704e1c8605486c3d9a27f267db13
KauanBS1 <kauan.santos@sptech.school>
2025-11-24 15:04:05 -0300

    Organizando ambiente para Quiz e Dashboard

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -7,23 +7,13 @@ comandos para mysql server
 */
 
 CREATE DATABASE singularity;
-
 USE singularity;
 
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
-);
-
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	id_usuario INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+	senha VARCHAR(50)
 );
 
 CREATE TABLE aviso (
